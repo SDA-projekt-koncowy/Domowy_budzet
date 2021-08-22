d06f5462fbb9063b41a4767a692ea324fffa5f38
KarolinaKL <karolina.krawczak@gmail.com>
2021-08-22 18:09:44 +0200

    Add income model

--- home_budget/budget/models.py
+++ home_budget/budget/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Income(models.Model):
+    amount = models.IntegerField()
+    description = models.TextField(blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Amount: {self.amount} - {self.description}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    income = models.ForeignKey(Income, on_delete=models.CASCADE, related_name='categories', null=True, blank=True)
