cdde6b97ea636bd4adb5078805b6391edbf23392
Stanislaw <raczkiewicz.stas@gmail.com>
2021-08-22 15:57:51 +0200

    Add Expense model

--- home_budget/budget/models.py
+++ home_budget/budget/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Category(models.Model):
+    pass
+
+
+class Expense(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.CharField(max_length=128)
+    date = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
