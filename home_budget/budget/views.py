312729fa9950165fa02035bd95f3e0c422104ab5
KarolinaKL <karolina.krawczak@gmail.com>
2021-08-22 18:10:31 +0200

    Add Income List View

--- home_budget/budget/views.py
+++ home_budget/budget/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView
+from budget.models import Category, Income
 # Create your views here.
+
+class IncomeListView(ListView):
+    template_name = "list.html"
+    model = Income
