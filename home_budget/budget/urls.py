f1beaa90834f23d525577193b56bcf0e8987ab91
KarolinaKL <karolina.krawczak@gmail.com>
2021-08-25 14:53:58 +0200

    move Income paths to the application folder

--- home_budget/budget/urls.py
+++ home_budget/budget/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 from budget import views
 
 
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('copyright/', views.copyright, name='copyright'),
+    path('income-list-view/', views.IncomeListView.as_view(), name="income-list-view"),
+    path('income-create-view/', views.IncomeCreateView.as_view(), name="income-create-view"),
+    path('income-update-view/<pk>/', views.IncomeUpdateView.as_view(), name="income-update-view"),
+    path('income-delete-view/<pk>/', views.IncomeDeleteView.as_view(), name="income-delete-view"),
+    path('income-detail-view/<pk>/', views.IncomeDetailView.as_view(), name="income-detail-view")
 ]
