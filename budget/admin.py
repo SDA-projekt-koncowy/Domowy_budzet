4c37ab212fe8761bdcd38fe41033026336bf98c1
Stanislaw <raczkiewicz.stas@gmail.com>
2021-08-26 17:42:40 +0200

    Add accounts app

--- budget/admin.py
+++ budget/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from budget.models import Category, Income
+from budget.models import Category, Income, Expense
 
 admin.site.register(Income)
+admin.site.register(Expense)
 admin.site.register(Category)
--- home_budget/settings.py
+++ home_budget/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'budget',
+    'accounts',
 ]
 
 MIDDLEWARE = [
