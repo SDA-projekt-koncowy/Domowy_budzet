75e20742637d449d3cb3f6247cbdbc981f65e179
Tomasz_Lupinski <tomasz.lupinski87@gmail.com>
2021-08-26 19:37:44 +0200

    Add settings view to base-template

--- budget/urls.py
+++ budget/urls.py
@@ -4,7 +4,6 @@ from budget import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('copyright/', views.copyright, name='copyright'),
     path('expense-list-view/', views.ExpenseListView.as_view(), name="expense-list-view"),
     path('expense-create-view/', views.ExpenseCreateView.as_view(), name="expense-create-view"),
     path('expense-update-view/<pk>/', views.ExpenseUpdateView.as_view(), name="expense-update-view"),
