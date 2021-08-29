6591ec55c1392510196596b9a0ed1e38ded6f279
Tomasz_Lupinski <tomasz.lupinski87@gmail.com>
2021-08-29 22:22:37 +0200

    Change password, settings views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from accounts import views
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('change-password', PasswordChangeView.as_view(), name='change-password'),
 ]
